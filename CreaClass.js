8a4b76b6b572c1ccf626cbaf0298ca256fbea9bf
Nikita <nik.europe@gmail.com>
2017-06-14 16:46:56 +0200

    Create CreaClass.js

---
+++ CreaClass.js
@@ -0,0 +1,54 @@
+(function ($) {
+
+    $(document).ready(function() {
+
+        $('.section-edit-profil .arrow-down').click(function(event) {
+
+            event.preventDefault();
+
+            $('#edit-profil-nav').toggleClass('open');
+
+            $('.arrow-down').toggleClass('open');
+
+        });
+
+
+        $('.edit-demandes-nav .arrow-down').click(function(event) {
+
+            event.preventDefault();
+
+            $('.edit-demandes-nav .arrow-down').toggleClass('open');
+
+            $('.edit-demandes-nav ul').toggleClass('open');
+
+        });
+
+
+
+        $('#block-navigationprincipale-3 .arrow-down').click(function(event) {
+
+            event.preventDefault();
+
+            $('#block-navigationprincipale-3 .arrow-down').toggleClass('open');
+
+            $('#block-navigationprincipale-3 ul').toggleClass('open');
+
+        });
+
+// Example de creation de class pour le mobile-menu-burger avec class open
+
+        $('.mobile-menu-burger').click(function(event) {
+
+            event.preventDefault();
+
+            $('.mobile-menu-burger').toggleClass('open');
+
+            $('#block-navigationprincipale-4 ul').toggleClass('open');
+
+        });
+
+
+    });
+
+}(jQuery));
+
